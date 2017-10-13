390fbb88bae71683d79b700b890ed17925288765
lmturner20 <LMT72@pitt.edu>
2017-10-12 20:51:44 -0400

    Update all_roc_curves.py

--- all_roc_curves.py
+++ all_roc_curves.py
@@ -4,131 +4,131 @@ import matplotlib.pyplot as plt
 import sklearn, argparse
 import sklearn.metrics
 
-2npncf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_2mod_nopool_noconcat.auc.finaltest"
-2npwcf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_2mod_nopool_wconcat.auc.finaltest"
-2wpncf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_2mod_wpool_noconcat.auc.finaltest"
-2wpwcf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_2mod_wpool_wconcat.auc.finaltest"
-3npncf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_3mod_nopool_noconcat.auc.finaltest"
-#3npwcf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_3mod_nopool_wconcat.auc.finaltest"
-#3wpncf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_3mod_wpool_noconcat.auc.finaltest"
-3wpwcf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_3mod_wpool_wconcat.auc.finaltest"
+_2npncf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_2mod_nopool_noconcat.auc.finaltest"
+_2npwcf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_2mod_nopool_wconcat.auc.finaltest"
+_2wpncf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_2mod_wpool_noconcat.auc.finaltest"
+_2wpwcf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_2mod_wpool_wconcat.auc.finaltest"
+_3npncf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_3mod_nopool_noconcat.auc.finaltest"
+#_3npwcf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_3mod_nopool_wconcat.auc.finaltest"
+#_3wpncf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_3mod_wpool_noconcat.auc.finaltest"
+_3wpwcf = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/dn_3mod_wpool_wconcat.auc.finaltest"
 outprefix = "/net/pulsar/home/koes/lmt72/DenseNets/DenseNets/8_roc_curves"
 
-2npnctrue = [] #dn_2mod_nopool_noconcat
-2npncscore = []
-2npncfile = open(2npncf,'r')
-for line in 2npncfile:
+_2npnctrue = [] #dn_2mod_nopool_noconcat
+_2npncscore = []
+_2npncfile = open(_2npncf,'r')
+for line in _2npncfile:
     if line.startswith('#'):
         pass
     else:
         data= line.split()
-        2npnctrue.append( bool(float(data[0])) )
-        2npncscore.append( float(data[1].strip()) )
-2npncfpr, 2npnctpr, _ = sklearn.metrics.roc_curve(2npnctrue,2npncscore)
-2npnc = sklearn.metrics.roc_auc_score(2npnctrue,2npncscore)
+        _2npnctrue.append( bool(float(data[0])) )
+        _2npncscore.append( float(data[1].strip()) )
+_2npncfpr, _2npnctpr, _ = sklearn.metrics.roc_curve(_2npnctrue,_2npncscore)
+_2npnc = sklearn.metrics.roc_auc_score(_2npnctrue,_2npncscore)
 
-2npwctrue = [] #dn_2mod_nopool_wconcat
-2npwcscore = []
-2npwcfile = open(2npwcf,'r')
-for line in 2npwcfile:
+_2npwctrue = [] #dn_2mod_nopool_wconcat
+_2npwcscore = []
+_2npwcfile = open(_2npwcf,'r')
+for line in _2npwcfile:
     if line.startswith('#'):
         pass
     else:
         data= line.split()
-        2npwctrue.append( bool(float(data[0])) )
-        2npwcscore.append( float(data[1].strip()) )
-2npwcfpr, 2npwctpr, _ = sklearn.metrics.roc_curve(2npwctrue,2npwcscore)
-2npwc = sklearn.metrics.roc_auc_score(2npwctrue,2npwcscore)
+        _2npwctrue.append( bool(float(data[0])) )
+        _2npwcscore.append( float(data[1].strip()) )
+_2npwcfpr, _2npwctpr, _ = sklearn.metrics.roc_curve(_2npwctrue,_2npwcscore)
+_2npwc = sklearn.metrics.roc_auc_score(_2npwctrue,_2npwcscore)
 
-2wpnctrue = [] #dn_2mod_wpool_noconcat
-2wpncscore = []
-2wpncfile = open(2wpncf,'r')
-for line in 2wpncfile:
+_2wpnctrue = [] #dn_2mod_wpool_noconcat
+_2wpncscore = []
+_2wpncfile = open(_2wpncf,'r')
+for line in _2wpncfile:
     if line.startswith('#'):
         pass
     else:
         data= line.split()
-        2wpnctrue.append( bool(float(data[0])) )
-        2wpncscore.append( float(data[1].strip()) )
-2wpncfpr, 2wpnctpr, _ = sklearn.metrics.roc_curve(2wpnctrue,2wpncscore)
-2wpnc = sklearn.metrics.roc_auc_score(2wpnctrue,2wpncscore)
+        _2wpnctrue.append( bool(float(data[0])) )
+        _2wpncscore.append( float(data[1].strip()) )
+_2wpncfpr, _2wpnctpr, _ = sklearn.metrics.roc_curve(_2wpnctrue,_2wpncscore)
+_2wpnc = sklearn.metrics.roc_auc_score(_2wpnctrue,_2wpncscore)
 
-2wpwctrue = [] #dn_2mod_wpool_wconcat
-2wpwcscore = []
-2wpwcfile = open(2wpwcf,'r')
-for line in 2wpwcfile:
+_2wpwctrue = [] #dn_2mod_wpool_wconcat
+_2wpwcscore = []
+_2wpwcfile = open(_2wpwcf,'r')
+for line in _2wpwcfile:
     if line.startswith('#'):
         pass
     else:
         data= line.split()
-        2wpwctrue.append( bool(float(data[0])) )
-        2wpwcscore.append( float(data[1].strip()) )
-2wpwcfpr, 2wpwctpr, _ = sklearn.metrics.roc_curve(2wpwctrue,2wpwcscore)
-2wpwc = sklearn.metrics.roc_auc_score(2wpwctrue,2wpwcscore)
+        _2wpwctrue.append( bool(float(data[0])) )
+        _2wpwcscore.append( float(data[1].strip()) )
+_2wpwcfpr, _2wpwctpr, _ = sklearn.metrics.roc_curve(_2wpwctrue,_2wpwcscore)
+_2wpwc = sklearn.metrics.roc_auc_score(_2wpwctrue,_2wpwcscore)
 
-3npnctrue = [] #dn_3mod_nopool_noconcat
-3npncscore = []
-3npncfile = open(3npncf,'r')
-for line in 3npncfile:
+_3npnctrue = [] #dn_3mod_nopool_noconcat
+_3npncscore = []
+_3npncfile = open(_3npncf,'r')
+for line in _3npncfile:
     if line.startswith('#'):
         pass
     else:
         data= line.split()
-        3npnctrue.append( bool(float(data[0])) )
-        3npncscore.append( float(data[1].strip()) )
-3npncfpr, 3npnctpr, _ = sklearn.metrics.roc_curve(3npnctrue,3npncscore)
-3npnc = sklearn.metrics.roc_auc_score(3npnctrue,3npncscore)
+        _3npnctrue.append( bool(float(data[0])) )
+        _3npncscore.append( float(data[1].strip()) )
+_3npncfpr, _3npnctpr, _ = sklearn.metrics.roc_curve(_3npnctrue,_3npncscore)
+_3npnc = sklearn.metrics.roc_auc_score(_3npnctrue,_3npncscore)
 
-#3npwctrue = [] #dn_3mod_nopool_wconcat
-#3npwcscore = []
-#3npwcfile = open(3npwcf,'r')
-#for line in 3npwcfile:
+#_3npwctrue = [] #dn_3mod_nopool_wconcat
+#_3npwcscore = []
+#_3npwcfile = open(_3npwcf,'r')
+#for line in _3npwcfile:
 #    if line.startswith('#'):
 #        pass
 #    else:
 #        data= line.split()
-#        3npwctrue.append( bool(float(data[0])) )
-#        3npwcscore.append( float(data[1].strip()) )
-#3npwcfpr, 3npwctpr, _ = sklearn.metrics.roc_curve(3npwctrue,3npwcscore)
-#3npwc = sklearn.metrics.roc_auc_score(3npwctrue,3npwcscore)
+#        _3npwctrue.append( bool(float(data[0])) )
+#        _3npwcscore.append( float(data[1].strip()) )
+#_3npwcfpr, _3npwctpr, _ = sklearn.metrics.roc_curve(_3npwctrue,_3npwcscore)
+#_3npwc = sklearn.metrics.roc_auc_score(_3npwctrue,_3npwcscore)
 
-#3wpnctrue = [] #dn_3mod_wpool_noconcat3
-#3wpncscore = []
-#3wpncfile = open(3wpncf,'r')
-#for line in 3wpncfile:
+#_3wpnctrue = [] #dn_3mod_wpool_noconcat3
+#_3wpncscore = []
+#_3wpncfile = open(_3wpncf,'r')
+#for line in _3wpncfile:
 #    if line.startswith('#'):
 #        pass
 #    else:
 #        data= line.split()
-#        3wpnctrue.append( bool(float(data[0])) )
-#        3wpncscore.append( float(data[1].strip()) )
-#3wpncfpr, 3wpnctpr, _ = sklearn.metrics.roc_curve(3wpnctrue,3wpncscore)
-#3wpnc = sklearn.metrics.roc_auc_score(3wpnctrue,3wpncscore)
+#        _3wpnctrue.append( bool(float(data[0])) )
+#        _3wpncscore.append( float(data[1].strip()) )
+#_3wpncfpr, _3wpnctpr, _ = sklearn.metrics.roc_curve(_3wpnctrue,_3wpncscore)
+#_3wpnc = sklearn.metrics.roc_auc_score(_3wpnctrue,_3wpncscore)
 
-3wpwctrue = [] #dn_3mod_wpool_wconcat
-3wpwcscore = []
-3wpwcfile = open(3wpwcf,'r')
-for line in 3wpwcfile:
+_3wpwctrue = [] #dn_3mod_wpool_wconcat
+_3wpwcscore = []
+_3wpwcfile = open(_3wpwcf,'r')
+for line in _3wpwcfile:
     if line.startswith('#'):
         pass
     else:
         data= line.split()
-        3wpwctrue.append( bool(float(data[0])) )
-        3wpwcscore.append( float(data[1].strip()) )
-3wpwcfpr, 3wpwctpr, _ = sklearn.metrics.roc_curve(3wpwctrue,3wpwcscore)
-3wpwc = sklearn.metrics.roc_auc_score(3wpwctrue,3wpwcscore)
+        _3wpwctrue.append( bool(float(data[0])) )
+        _3wpwcscore.append( float(data[1].strip()) )
+_3wpwcfpr, _3wpwctpr, _ = sklearn.metrics.roc_curve(_3wpwctrue,_3wpwcscore)
+_3wpwc = sklearn.metrics.roc_auc_score(_3wpwctrue,_3wpwcscore)
 
 
 fig = plt.figure(figsize=(8,8))
-plt.plot(2npncfpr,2npnctpr,label='2NPNC (AUC=%.4f)'%(2npnc),linewidth=8,color='b')
-plt.plot(2npwcfpr,2npwctpr,label='2NPWC (AUC=%.4f)'%(2npwc),linewidth=8,color='g')
-plt.plot(2wpncfpr,2wpnctpr,label='2WPNC (AUC=%.4f)'%(2wpnc),linewidth=8,color='r')
-plt.plot(2wpwcfpr,2wpwctpr,label='2WPWC (AUC=%.4f)'%(2wpwc),linewidth=8,color='c')
+plt.plot(_2npncfpr,_2npnctpr,label='2NPNC (AUC=%.4f)'%(_2npnc),linewidth=8,color='b')
+plt.plot(_2npwcfpr,_2npwctpr,label='2NPWC (AUC=%.4f)'%(_2npwc),linewidth=8,color='g')
+plt.plot(_2wpncfpr,_2wpnctpr,label='2WPNC (AUC=%.4f)'%(_2wpnc),linewidth=8,color='r')
+plt.plot(_2wpwcfpr,_2wpwctpr,label='2WPWC (AUC=%.4f)'%(_2wpwc),linewidth=8,color='c')
 
-plt.plot(3npncfpr,3npnctpr,label='3NPNC (AUC=%.4f)'%(3npnc),linewidth=8,color='m')
-#plt.plot(3npwcfpr,3npwctpr,label='3NPWC (AUC=%.4f)'%(3npwc),linewidth=8,color='y')
-#plt.plot(3wpncfpr,3wpnctpr,label='3WPNC (AUC=%.4f)'%(3wpnc),linewidth=8,color='k')
-plt.plot(3wpwcfpr,3wpwctpr,label='3WPWC (AUC=%.4f)'%(3wpwc),linewidth=8,color='w')
+plt.plot(_3npncfpr,_3npnctpr,label='3NPNC (AUC=%.4f)'%(_3npnc),linewidth=8,color='m')
+#plt.plot(_3npwcfpr,_3npwctpr,label='3NPWC (AUC=%.4f)'%(_3npwc),linewidth=8,color='y')
+#plt.plot(_3wpncfpr,_3wpnctpr,label='3WPNC (AUC=%.4f)'%(_3wpnc),linewidth=8,color='k')
+plt.plot(_3wpwcfpr,_3wpwctpr,label='3WPWC (AUC=%.4f)'%(_3wpwc),linewidth=8,color='w')
 plt.legend(loc='lower right',fontsize=20)
 plt.xlabel('False Positive Rate',fontsize=22)
 plt.ylabel('True Positive Rate',fontsize=22)
