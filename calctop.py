1cdf2c2402f9a1ecd7759751f8c64cd31ecef7d2
lmturner20 <LMT72@pitt.edu>
2017-10-23 14:36:25 -0400

    Create calctop.py

---
+++ calctop.py
@@ -0,0 +1,84 @@
+from Bio.PDB.PDBParser import PDBParser
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m','--model',type=str,required=True)
+    parser.add_argument('-p','--prefix',type=str,required=True) #same for caffemodel and test sets
+    modelname = model
+
+    testfile = (prefix + ".0.rmsd.finaltest")
+    caffemodel = (prefix + ".0_iter_100000.caffemodel")
+    result = evaluate_fold(testfile, caffemodel, modelname)
+    print (result)
+
+    testfile = (prefix + ".1.rmsd.finaltest")
+    caffemodel = (prefix + ".1_iter_100000.caffemodel")
+    result = evaluate_fold(testfile, caffemodel, modelname)
+    print (result)
+
+    testfile = (prefix + ".2.rmsd.finaltest")
+    caffemodel = (prefix + ".2_iter_100000.caffemodel")
+    result = evaluate_fold(testfile, caffemodel, modelname)
+    print (result)
+
+def evaluate_fold(testfile, caffemodel, modelname):
+    '''Evaluate the passed model and the specified test set.
+    Assumes the .model file is named a certain way.
+    Returns tuple:
+    (correct, prediction, receptor, ligand, label (optional), posescore (optional))
+    label and posescore are only provided is trained on pose data
+    ''' 
+    caffe.set_mode_gpu()
+    test_model = 'predict.%d.prototxt' % os.getpid()
+    train.write_model_file(test_model, modelname, testfile, testfile, '')
+    test_net = caffe.Net(test_model, caffemodel, caffe.TEST)
+    lines = open(testfile).readlines()
+
+    res = None
+    i = 0 #index in batch
+    correct = 0
+    prediction = 0
+    receptor = ''
+    ligand = ''
+    label = 0
+    posescore = -1
+    ret = []
+    for line in lines:
+        #check if we need a new batch of results
+        if not res or i >= batch_size:
+            res = test_net.forward()
+            if 'output' in res:
+                batch_size = res['output'].shape[0]
+            else:
+                batch_size = res['affout'].shape[0]
+            i = 0
+
+        if 'labelout' in res:
+            label = float(res['labelout'][i])
+        if 'output' in res:
+            posescore = float(res['output'][i][1])
+        if 'affout' in res:
+            correct = float(res['affout'][i])
+        if 'predaff' in res:
+            prediction = float(res['predaff'][i])
+            if not np.isfinite(prediction).all():
+                os.remove(test_model)
+                return [] #gracefully handle nan?
+
+        #extract ligand/receptor for input file
+        tokens = line.split()
+        for t in xrange(len(tokens)):
+            if tokens[t].endswith('gninatypes'):
+                receptor = tokens[t]
+                ligand = tokens[t+1]
+                break
+        
+        #(correct, prediction, receptor, ligand, label (optional), posescore (optional))       
+        if posescore < 0:
+            ret.append((correct, prediction, receptor, ligand))
+        else:
+            ret.append((correct, prediction, receptor, ligand, label, posescore))
+            
+        i += 1 #batch index
+        
+    os.remove(test_model)
+    return ret
